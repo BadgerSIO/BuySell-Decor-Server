dfad867eec256e8beb6b02454c42c0de6b16f991
BadgerSIO <saadibneomar@gmail.com>
2022-11-24 20:30:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("BuySell Decor server is running");
+});
+
+app.listen(port, () => console.log(`BuySell Decor portal running on ${port}`));
