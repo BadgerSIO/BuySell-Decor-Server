7f4e39b9441244b4abfb7abe140357ec0ce2d64e
BadgerSIO <saadibneomar@gmail.com>
2022-11-26 00:58:27 +0600

    api for verifying admin and seller added

--- index.js
+++ index.js
@@ -26,10 +26,45 @@ async function run() {
     const categoryCollections = client
       .db("buySellDecor")
       .collection("categories");
+    const userCollections = client.db("buySellDecor").collection("users");
+
+    //categories
     app.get("/categories", async (req, res) => {
       const query = {};
       const result = await categoryCollections.find(query).toArray();
       res.send(result);
+    });
+
+    //users collection
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {
+        email: user.email,
+      };
+      const alreadyExist = await userCollections.findOne(query);
+      if (alreadyExist) {
+        return res.send({ userExist: true });
+      }
+      const result = await userCollections.insertOne(user);
+      res.send(result);
+    });
+    //check admin
+    app.get("/user/admin/:email", async (req, res) => {
+      const reqEmail = req.params.email;
+      const query = {
+        email: reqEmail,
+      };
+      const user = await userCollections.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+    //check seller
+    app.get("/user/seller/:email", async (req, res) => {
+      const reqEmail = req.params.email;
+      const query = {
+        email: reqEmail,
+      };
+      const user = await userCollections.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
     });
   } finally {
   }
