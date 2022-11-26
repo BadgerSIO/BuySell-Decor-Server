561cdaff64f7d23a520181935967d2a819f21295
BadgerSIO <saadibneomar@gmail.com>
2022-11-26 12:06:13 +0600

    jwt added verify admin added get seller buyer added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,21 @@ require("dotenv").config();
 //midleware
 app.use(cors());
 app.use(express.json());
+//verify jwt token
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(403).send({ message: "Access forbiden" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 app.get("/", async (req, res) => {
   res.send("BuySell Decor server is running");
@@ -27,6 +43,19 @@ async function run() {
       .db("buySellDecor")
       .collection("categories");
     const userCollections = client.db("buySellDecor").collection("users");
+    //verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      console.log(decodedEmail);
+      const query = {
+        email: decodedEmail,
+      };
+      const user = await userCollections.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      next();
+    };
 
     //categories
     app.get("/categories", async (req, res) => {
@@ -35,7 +64,7 @@ async function run() {
       res.send(result);
     });
 
-    //users collection
+    //add to users collections
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = {
@@ -48,6 +77,13 @@ async function run() {
       const result = await userCollections.insertOne(user);
       res.send(result);
     });
+    //delete user
+    app.delete(
+      "/deleteuser/:id",
+      verifyJWT,
+      verifyAdmin,
+      async (req, res) => {}
+    );
     //check admin
     app.get("/user/admin/:email", async (req, res) => {
       const reqEmail = req.params.email;
@@ -65,6 +101,33 @@ async function run() {
       };
       const user = await userCollections.findOne(query);
       res.send({ isSeller: user?.role === "seller" });
+    });
+
+    //get all sellers
+    app.get("/getuser", verifyJWT, verifyAdmin, async (req, res) => {
+      const reqrole = req.query.role;
+      const query = {
+        role: reqrole,
+      };
+      const result = await userCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // get jwt token
+    app.get("/jwT", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = {
+        email: email,
+      };
+      const user = await userCollections.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "6h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
   } finally {
   }
